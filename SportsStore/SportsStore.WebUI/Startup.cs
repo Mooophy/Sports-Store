def091fd4a959d1f485b9e88c889f3bc748f97a7
moophy <MoophyAlan@gmail.com>
2015-12-08 23:45:35 +1300

    setup file structure

---
+++ SportsStore/SportsStore.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportsStore.WebUI.Startup))]
+namespace SportsStore.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
