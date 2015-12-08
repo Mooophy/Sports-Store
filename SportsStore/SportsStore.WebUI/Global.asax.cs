1493681e95abc9fd4f955c6ccbdd1de74b23f789
moophy <MoophyAlan@gmail.com>
2015-12-09 02:33:04 +1300

    added class Product from listing 7-3

--- SportsStore/SportsStore.WebUI/Global.asax.cs
+++ SportsStore/SportsStore.WebUI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportsStore.WebUI.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace SportsStore.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            // put bindings here
+        }
+    }
+}
