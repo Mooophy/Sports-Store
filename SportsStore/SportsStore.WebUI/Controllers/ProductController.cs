27949ccd26b986982e6132ec2662d8c247e5c8cd
moophy <MoophyAlan@gmail.com>
2015-12-14 00:17:31 +1300

    Listing 7-6

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+        public ProductController(IProductsRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+    }
+}
