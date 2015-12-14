07df69c3f0257094260b755cdb96dec9a748e7ac
moophy <MoophyAlan@gmail.com>
2015-12-14 20:23:26 +1300

    7-15

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -11,13 +11,19 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductsRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductsRepository productRepository)
         {
             repository = productRepository;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            var products_of_one_page = repository
+                .Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize);
+            return View(products_of_one_page);
         }
     }
 }
