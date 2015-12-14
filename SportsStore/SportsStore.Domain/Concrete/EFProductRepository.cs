82f18efd25fd19abe0069ec26831c4f99b968423
moophy <MoophyAlan@gmail.com>
2015-12-14 20:02:11 +1300

    7-13

---
+++ SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductsRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IQueryable<Product> Products
+        {
+            get { return this.context.Products; }
+        }
+    }
+}
