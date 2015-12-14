2ab48fe32ed897b16fca71b8a8cc4a06ac9a7e1b
moophy <MoophyAlan@gmail.com>
2015-12-14 23:24:11 +1300

    HtmlHelpers with Extension method

---
+++ SportsStore/SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl)
+        {
+            var result = new StringBuilder();
+            for(int i = 1; i<= pagingInfo.TotalPages; ++i)
+            {
+                var tag = new TagBuilder("a"); //construct an <a> tag
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pagingInfo.CurrentPage)
+                    tag.AddCssClass("selected");
+                result.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
